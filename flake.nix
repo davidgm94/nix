ef9b682a0342ffc53613c9f8b2ecfbd9c2b7e6ad
David Gonzalez Martin <davidgm94.work@protonmail.com>
2022-08-02 15:26:27 +0000

    First commit

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+    description = "David Gonzalez Martin's nix configuration";
+
+    inputs =
+    {
+        nixpkgs.url = "nixpkgs/nixos-22.05";
+	nixos-wsl.url = "github:nix-community/nixos-wsl/main";
+    };
+
+    outputs = { self, nixpkgs, nixos-wsl, ... } @attrs: 
+    let
+        wsl2_x86_64_gui_hostname = "wsl2-x86-gui";
+	user = "david";
+    in
+    {
+	nixosConfigurations.${wsl2_x86_64_gui_hostname} = nixpkgs.lib.nixosSystem {
+	    system = "x86_64-linux";
+	    specialArgs = attrs;
+	    modules = [
+	        ./configuration.nix
+		nixos-wsl.nixosModules.wsl {
+		wsl = {
+		    enable = true;
+		    automountPath = "/mnt";
+		    defaultUser = "${user}";
+		    startMenuLaunchers = true;
+		    wslConf.network.hostname = "${wsl2_x86_64_gui_hostname}";
+		    };
+		}
+	    ];
+	};
+    };
+}
