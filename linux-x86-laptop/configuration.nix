1237fe8b05127d9e4eceacfd2f93422eaa8069d5
David Gonzalez Martin <davidgm94.work@protonmail.com>
2022-08-03 14:53:17 +0000

    Restore laptop configuration validity

--- linux-x86-laptop/configuration.nix
+++ linux-x86-laptop/configuration.nix
@@ -18,13 +18,6 @@ in
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
-  networking.hostName = "linux-x86-laptop"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Enable networking
   networking.networkmanager.enable = true;
 
@@ -45,6 +38,7 @@ in
     LC_TELEPHONE = "es_ES.utf8";
     LC_TIME = "es_ES.utf8";
   };
+
   services.xserver = {
     enable = true;   
     desktopManager = {
@@ -64,9 +58,6 @@ in
     # Enable touchpad support (enabled default in most desktopManager).
     libinput.enable = true;
   };
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
 
   # More Nvidia configuration
   hardware.opengl.enable = true;
@@ -91,85 +82,4 @@ in
     # no need to redefine it in your config for now)
     #media-session.enable = true;
   };
-
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.${username} = {
-    isNormalUser = true;
-    extraGroups = [ "networkmanager" "wheel" ];
-  };
-
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    xfce.xfce4-whiskermenu-plugin
-    xfce.xfce4-pulseaudio-plugin
-    zsh
-    firefox
-    neovim
-    signal-desktop
-    tdesktop
-    discord
-    git
-    lm_sensors
-    htop
-    alacritty
-    kitty
-    wezterm
-    gdb
-    ripgrep
-    qemu_full
-    rustup
-    rust-analyzer
-    mpv
-    vlc
-    smplayer
-    nodejs
-    tokei
-    bat
-    bash-completion
-    zsh-completions
-    libreoffice
-    xorriso
-    cmake
-    fira
-    fira-mono
-    fira-code
-    dejavu_fonts
-    liberation_ttf
-    noto-fonts
-    man
-    xclip
-    pciutils
-    gparted
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.05"; # Did you read the comment?
-
 }
