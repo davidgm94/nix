6f333c47e98625533fe8b4c5664c1226d55d24ee
David Gonzalez Martin <davidgm94.work@protonmail.com>
2022-08-03 20:14:40 +0200

    Cleanup config

--- system/linux-x86-laptop/configuration.nix
+++ system/linux-x86-laptop/configuration.nix
@@ -16,7 +16,7 @@ in
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.efi.efiSysMountPoint = "/boot";
 
   networking.hostName = "linux-x86-laptop"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
