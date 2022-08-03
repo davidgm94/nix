df99f2457443d22592285194f7d0f500feafd440
David Gonzalez Martin <davidgm94.work@protonmail.com>
2022-08-03 19:39:02 +0200

    Correct EFI filesystem root

--- linux-x86-laptop/configuration.nix
+++ linux-x86-laptop/configuration.nix
@@ -16,7 +16,7 @@ in
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.efi.efiSysMountPoint = "/boot";
 
   # Enable networking
   networking.networkmanager.enable = true;
